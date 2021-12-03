5363c056315d804b053bbcada7ec0b2054c55ec7
Michal Dengusiak <tassimulation@gmail.com>
2021-12-03 11:33:29 +0000

    done! :)

---
+++ Grasshopper/SAM.Core.Grasshopper.Psychrometrics/Component/SAMPsychrometricsPsychrometrics.cs
@@ -0,0 +1,245 @@
+﻿using Grasshopper.Kernel;
+using SAM.Core.Grasshopper.Psychrometrics.Properties;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMPsychrometricsPsychrometrics : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("064dc9f2-a368-459a-9e97-b60588a269f1");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_dryBulbTemperature", NickName = "_dryBulbTemperature", Description = "Dry bulb temperature [°C]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_relativeHumidity_", NickName = "_relativeHumidity", Description = "Relative humidity (0 - 100) [%] \n Connect only one humidity indication \n relativeHumidity or wetBulbTemperature or dewPointTemperature", Access = GH_ParamAccess.item, Optional = true }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_wetBulbTemperature_", NickName = "_wetBulbTemperature_", Description = "Wet bulb temperature [°C] \n Connect only one humidity indication \n relativeHumidity or wetBulbTemperature or dewPointTemperature", Access = GH_ParamAccess.item, Optional = true }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_dewPointTemperature_", NickName = "_dewPointTemperature_", Description = "Dew Point Temperature [°C] \n Connect only one humidity indication \n relativeHumidity or wetBulbTemperature or dewPointTemperature", Access = GH_ParamAccess.item, Optional = true }, ParamVisibility.Binding));
+
+                global::Grasshopper.Kernel.Parameters.Param_Number param_Number = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_pressure_", NickName = "_pressure_", Description = "Atmospheric pressure [Pa]", Access = GH_ParamAccess.item, Optional = true };
+                param_Number.SetPersistentData(101325);
+                result.Add(new GH_SAMParam(param_Number, ParamVisibility.Voluntary));
+
+                return result.ToArray();
+            }
+        }
+
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "dryBulbTemperature", NickName = "dryBulbTemperature", Description = "Dry bulb temperature [°C]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "relativeHumidity", NickName = "relativeHumidity", Description = "Dry bulb temperature [°C]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "humidityRatio", NickName = "humidityRatio", Description = "Relative humidity (0 - 100) [%]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "wetBulbTemperature", NickName = "wetBulbTemperature", Description = "Wet bulb temperature [°C]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "dewPointTemperature", NickName = "dewPointTemperature", Description = "Dew Point Temperature [°C]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "vapourPressure", NickName = "vapourPressure", Description = "Saturation Vapour Pressure [Pa]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "enthalpy", NickName = "enthalpy", Description = "Enthalpy [J/kg]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "specificVolume", NickName = "specificVolume", Description = "Specific Volume [m³/kg]", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "degreeSaturation", NickName = "degreeSaturation", Description = "Degree of saturation [unitless]", Access = GH_ParamAccess.item, Optional = true }, ParamVisibility.Voluntary));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "pressure", NickName = "pressure", Description = "Atmospheric pressure [Pa]", Access = GH_ParamAccess.item, Optional = true }, ParamVisibility.Voluntary));
+
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Updates PanelTypes for AdjacencyCluster
+        /// </summary>
+        public SAMPsychrometricsPsychrometrics()
+          : base("SAMPsychrometrics.Psychrometrics", "SAMPsychrometrics.Psychrometrics",
+              "Utility function to calculate relative humidity, humidity ratio, wet - bulb temperature, dew - point temperature, \nvapour pressure, moist air enthalpy, moist air volume, and degree of saturation of air given \ndry-bulb temperature, (relative humidity or humidity ratio or wet bulb temperature) and pressure. \n*The degree of saturation (i.e humidity ratio of the air / humidity ratio of the air at saturationat the same temperature and pressure)\n Connect only one humidity indication",
+              "SAM", "Psychrometrics")
+        {
+        }
+
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            index = Params.IndexOfInputParam("_dryBulbTemperature");
+            if (index == -1)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            double dryBulbTemperature = double.NaN;
+            double relativeHumidity = double.NaN;
+            double humidityRatio = double.NaN;
+            double wetBulbTemperature = double.NaN;
+            double dewPointTemperature = double.NaN;
+            double vapourPressure = double.NaN;
+            double enthalpy = double.NaN;
+            double specificVolume = double.NaN;
+            double degreeSaturation = double.NaN;
+            double pressure = double.NaN;
+
+            index = Params.IndexOfInputParam("_dryBulbTemperature");
+            if (!dataAccess.GetData(index, ref dryBulbTemperature) || double.IsNaN(dryBulbTemperature))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfInputParam("_relativeHumidity_");
+            if (index == -1 || !dataAccess.GetData(index, ref relativeHumidity) || double.IsNaN(relativeHumidity))
+            {
+                relativeHumidity = double.NaN;
+            }
+
+            if(!double.IsNaN(relativeHumidity))
+            {
+                if(relativeHumidity < 0 || relativeHumidity > 100)
+                {
+                    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                    return;
+                }
+            }
+
+            index = Params.IndexOfInputParam("_wetBulbTemperature_");
+            if (index == -1 || !dataAccess.GetData(index, ref wetBulbTemperature) || double.IsNaN(wetBulbTemperature))
+            {
+                wetBulbTemperature = double.NaN;
+            }
+
+            index = Params.IndexOfInputParam("_dewPointTemperature_");
+            if (index == -1 || !dataAccess.GetData(index, ref dewPointTemperature) || double.IsNaN(dewPointTemperature))
+            {
+                dewPointTemperature = double.NaN;
+            }
+
+            if(double.IsNaN(relativeHumidity) && double.IsNaN(wetBulbTemperature) && double.IsNaN(dewPointTemperature))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfInputParam("_pressure_");
+            if (index == -1 || !dataAccess.GetData(index, ref pressure) || double.IsNaN(pressure))
+            {
+                pressure = 101325;
+            }
+
+            if (!double.IsNaN(relativeHumidity) && double.IsNaN(wetBulbTemperature) && double.IsNaN(dewPointTemperature))
+            {
+                 Core.Psychrometrics.Query.CalcPsychrometrics_ByRelativeHumidity(dryBulbTemperature, relativeHumidity, pressure, 
+                     out humidityRatio,
+                     out wetBulbTemperature,
+                     out dewPointTemperature,
+                     out vapourPressure,
+                     out enthalpy,
+                     out specificVolume,
+                     out degreeSaturation);
+            }
+            else if (double.IsNaN(relativeHumidity) && !double.IsNaN(wetBulbTemperature) && double.IsNaN(dewPointTemperature))
+            {
+                Core.Psychrometrics.Query.CalcPsychrometrics_ByTWetBulb(dryBulbTemperature, wetBulbTemperature, pressure,
+                     out humidityRatio,
+                     out relativeHumidity,
+                     out dewPointTemperature,
+                     out vapourPressure,
+                     out enthalpy,
+                     out specificVolume,
+                     out degreeSaturation);
+            }
+            else if (double.IsNaN(relativeHumidity) && double.IsNaN(wetBulbTemperature) && !double.IsNaN(dewPointTemperature))
+            {
+                Core.Psychrometrics.Query.CalcPsychrometrics_ByTDewPoint(dryBulbTemperature, dewPointTemperature, pressure,
+                     out humidityRatio,
+                     out wetBulbTemperature,
+                     out relativeHumidity,
+                     out vapourPressure,
+                     out enthalpy,
+                     out specificVolume,
+                     out degreeSaturation);
+            }
+            else
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfOutputParam("dryBulbTemperature");
+            if(index != -1)
+            {
+                dataAccess.SetData(index, dryBulbTemperature);
+            }
+
+            index = Params.IndexOfOutputParam("relativeHumidity");
+            if(index != -1)
+            {
+                dataAccess.SetData(index, relativeHumidity);
+            }
+
+            index = Params.IndexOfOutputParam("humidityRatio");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, humidityRatio);
+            }
+
+            index = Params.IndexOfOutputParam("wetBulbTemperature");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, wetBulbTemperature);
+            }
+
+            index = Params.IndexOfOutputParam("dewPointTemperature");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, dewPointTemperature);
+            }
+
+            index = Params.IndexOfOutputParam("vapourPressure");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, vapourPressure);
+            }
+
+            index = Params.IndexOfOutputParam("enthalpy");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, enthalpy);
+            }
+
+            index = Params.IndexOfOutputParam("specificVolume");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, specificVolume);
+            }
+
+            index = Params.IndexOfOutputParam("degreeSaturation");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, degreeSaturation);
+            }
+
+            index = Params.IndexOfOutputParam("pressure");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, pressure);
+            }
+        }
+    }
+}
--- SAM_Psychrometrics/SAM.Core.Psychrometrics/Query/CalcPsychrometric.cs
+++ SAM_Psychrometrics/SAM.Core.Psychrometrics/Query/CalcPsychrometric.cs
@@ -6,84 +6,138 @@ namespace SAM.Core.Psychrometrics
     public static partial class Query
     {
         /// <summary>
-        ///Utility function to calculate humidity ratio, dew-point temperature, relative humidity,
+        ///Utility function to calculate humidity ratio, wet-bulb temperature, dew-point temperature,
         /// vapour pressure, moist air enthalpy, moist air volume, and degree of saturation of air given
-        /// dry-bulb temperature, wet-bulb temperature, and pressure.
-        /// </summary>
-        /// <param name="dryBulbTemperature">Dry bulb temperature [°C]</param>
-        /// <param name="wetBulbTemperature">Wet bulb temperature [°C]</param>
-        /// <param name="pressure">Atmospheric pressure [Pa]</param>
-        /// <returns>list</returns>
-        public static List<double> CalcPsychrometrics_ByTWetBulb(double dryBulbTemperature, double wetBulbTemperature, double pressure)
-        {
-            List<double> result = new List<double>();
-            PsychroLib.Psychrometrics psychrometrics = new PsychroLib.Psychrometrics(PsychroLib.UnitSystem.SI);
-
-            double HumRatio = psychrometrics.GetHumRatioFromTWetBulb(dryBulbTemperature,wetBulbTemperature,pressure);
-            double TDewPoint = psychrometrics.GetTDewPointFromHumRatio(dryBulbTemperature, HumRatio, pressure);
-            double RelHum = psychrometrics.GetRelHumFromHumRatio(dryBulbTemperature, HumRatio, pressure);
-            double VapPres = psychrometrics.GetVapPresFromHumRatio(HumRatio, pressure);
-            double MoistAirEnthalpy = psychrometrics.GetMoistAirEnthalpy(dryBulbTemperature, HumRatio);
-            double MoistAirVolume  = psychrometrics.GetMoistAirVolume(dryBulbTemperature, HumRatio, pressure);
-            double DegreeOfSaturation  = psychrometrics.GetDegreeOfSaturation(dryBulbTemperature, HumRatio, pressure);
-
-            result.AddRange(new List<double>() { HumRatio, TDewPoint, RelHum, VapPres, MoistAirEnthalpy, MoistAirVolume, DegreeOfSaturation });
-
-            return result; 
-        }
-
-        /// <summary>
-        ///Utility function to calculate humidity ratio, wet-bulb temperature, relative humidity,
-        /// vapour pressure, moist air enthalpy, moist air volume, and degree of saturation of air given
-        /// dry-bulb temperature, dew-point temperature, and pressure.
-        /// </summary>
-        /// <param name="dryBulbTemperature">Dry bulb temperature [°C]</param>
-        /// <param name="dewPointTemperature">Dew point temperature [°C]</param>
-        /// <param name="pressure">Atmospheric pressure [Pa]</param>
-        /// <returns>list</returns>
-        public static List<double> CalcPsychrometrics_ByTDewPoint(double dryBulbTemperature, double dewPointTemperature, double pressure)
-        {
-            List<double> result = new List<double>();
-            PsychroLib.Psychrometrics psychrometrics = new PsychroLib.Psychrometrics(PsychroLib.UnitSystem.SI);
-
-            double HumRatio = psychrometrics.GetHumRatioFromTDewPoint(dewPointTemperature, pressure);
-            double TWetBulb = psychrometrics.GetTWetBulbFromHumRatio(dryBulbTemperature, HumRatio, pressure);
-            double RelHum = psychrometrics.GetRelHumFromHumRatio(dryBulbTemperature, HumRatio, pressure);
-            double VapPres = psychrometrics.GetVapPresFromHumRatio(HumRatio, pressure);
-            double MoistAirEnthalpy = psychrometrics.GetMoistAirEnthalpy(dryBulbTemperature, HumRatio);
-            double MoistAirVolume = psychrometrics.GetMoistAirVolume(dryBulbTemperature, HumRatio, pressure);
-            double DegreeOfSaturation = psychrometrics.GetDegreeOfSaturation(dryBulbTemperature, HumRatio, pressure);
-
-            result.AddRange(new List<double>() { HumRatio, TWetBulb, RelHum, VapPres, MoistAirEnthalpy, MoistAirVolume, DegreeOfSaturation });
-
-            return result;
-        }
-
-        /// <summary>
-        ///Utility function to calculate humidity ratio, wet-bulb temperature, relative humidity,
-        /// vapour pressure, moist air enthalpy, moist air volume, and degree of saturation of air given
-        /// dry-bulb temperature, dew-point temperature, and pressure.
+        /// dry-bulb temperature, relative humidity and pressure.
+        /// *The degree of saturation (i.e humidity ratio of the air / humidity ratio of the air at saturationat the same temperature and pressure)
         /// </summary>
         /// <param name="dryBulbTemperature">Dry bulb temperature [°C]</param>
         /// <param name="relativeHumidity">Relative humidity (0 - 100) [%]</param>
         /// <param name="pressure">Atmospheric pressure [Pa]</param>
+        /// <param name="humidityRatio">Humidity Ratio [kg_waterVapor/kg_dryAir]</param>
+        /// <param name="wetBulbTemperature">>Wet bulb temperature [°C]</param>
+        /// <param name="dewPointTemperature">Dew Point Temperature [°C]</param>
+        /// <param name="vapourPressure">Saturation Vapour Pressure [Pa]</param>
+        /// <param name="enthalpy">Enthalpy [J/kg]</param>
+        /// <param name="specificVolume">Specific Volume [m³/kg]</param>
+        /// <param name="degreeSaturation">Degree of saturation [unitless]</param>
         /// <returns>list</returns>
-        public static List<double> CalcPsychrometrics_ByRelativeHumidity(double dryBulbTemperature, double relativeHumidity, double pressure)
+        public static bool CalcPsychrometrics_ByRelativeHumidity(double dryBulbTemperature, double relativeHumidity, double pressure, out double humidityRatio, out double wetBulbTemperature, out double dewPointTemperature, out double vapourPressure, out double enthalpy, out double specificVolume, out double degreeSaturation)
         {
-            List<double> result = new List<double>();
+            humidityRatio = double.NaN;
+            wetBulbTemperature = double.NaN;
+            dewPointTemperature = double.NaN;
+            //relativeHumidity = double.NaN;
+            vapourPressure = double.NaN;
+            enthalpy = double.NaN;
+            specificVolume = double.NaN;
+            degreeSaturation = double.NaN;
+
             PsychroLib.Psychrometrics psychrometrics = new PsychroLib.Psychrometrics(PsychroLib.UnitSystem.SI);
+            if (psychrometrics == null)
+            {
+                return false;
+            }
 
-            double HumRatio = psychrometrics.GetHumRatioFromRelHum(dryBulbTemperature, relativeHumidity / 100, pressure);
-            double TWetBulb = psychrometrics.GetTWetBulbFromHumRatio(dryBulbTemperature, HumRatio, pressure);
-            double TDewPoint = psychrometrics.GetTDewPointFromHumRatio(dryBulbTemperature, HumRatio, pressure);
-            double VapPres = psychrometrics.GetVapPresFromHumRatio(HumRatio, pressure);
-            double MoistAirEnthalpy = psychrometrics.GetMoistAirEnthalpy(dryBulbTemperature, HumRatio);
-            double MoistAirVolume = psychrometrics.GetMoistAirVolume(dryBulbTemperature, HumRatio, pressure);
-            double DegreeOfSaturation = psychrometrics.GetDegreeOfSaturation(dryBulbTemperature, HumRatio, pressure);
+            humidityRatio = psychrometrics.GetHumRatioFromRelHum(dryBulbTemperature, relativeHumidity/100, pressure);
+            wetBulbTemperature = psychrometrics.GetTWetBulbFromHumRatio(dryBulbTemperature, humidityRatio, pressure);
+            dewPointTemperature = psychrometrics.GetTDewPointFromHumRatio(dryBulbTemperature, humidityRatio, pressure);
+            vapourPressure = psychrometrics.GetVapPresFromHumRatio(humidityRatio, pressure);
+            enthalpy = psychrometrics.GetMoistAirEnthalpy(dryBulbTemperature, humidityRatio);
+            specificVolume = psychrometrics.GetMoistAirVolume(dryBulbTemperature, humidityRatio, pressure);
+            degreeSaturation = psychrometrics.GetDegreeOfSaturation(dryBulbTemperature, humidityRatio, pressure);
 
-            result.AddRange(new List<double>() { HumRatio, TWetBulb, TDewPoint, VapPres, MoistAirEnthalpy, MoistAirVolume, DegreeOfSaturation });
-
-            return result;
+            return true;
         }
+
+        /// <summary>
+        ///Utility function to calculate humidity ratio, dew-point temperature, relative humidity,
+        /// vapour pressure, moist air enthalpy, moist air volume, and degree of saturation of air given
+        /// dry-bulb temperature, wet-bulb temperature, and pressure.
+        /// *The degree of saturation (i.e humidity ratio of the air / humidity ratio of the air at saturationat the same temperature and pressure)
+        /// </summary>
+        /// <param name="dryBulbTemperature">Dry bulb temperature [°C]</param>
+        /// <param name="wetBulbTemperature">Wet bulb temperature [°C]</param>
+        /// <param name="pressure">Atmospheric pressure [Pa]</param>
+        /// <param name="humidityRatio">Humidity Ratio [kg_waterVapor/kg_dryAir]</param>
+        /// <param name="relativeHumidity">Relative humidity (0 - 100) [%]</param>
+        /// <param name="dewPointTemperature">Dew Point Temperature [°C]</param>
+        /// <param name="vapourPressure">Saturation Vapour Pressure [Pa]</param>
+        /// <param name="enthalpy">Enthalpy [J/kg]</param>
+        /// <param name="specificVolume">Specific Volume [m³/kg]</param>
+        /// <param name="degreeSaturation">Degree of saturation [unitless]</param>
+        /// <returns>list</returns>
+        public static bool CalcPsychrometrics_ByTWetBulb(double dryBulbTemperature, double wetBulbTemperature, double pressure, out double humidityRatio, out double relativeHumidity, out double dewPointTemperature, out double vapourPressure, out double enthalpy, out double specificVolume, out double degreeSaturation)
+        {
+            humidityRatio = double.NaN;
+            relativeHumidity = double.NaN;
+            dewPointTemperature = double.NaN;
+            vapourPressure = double.NaN;
+            enthalpy = double.NaN;
+            specificVolume = double.NaN;
+            degreeSaturation = double.NaN;
+
+            PsychroLib.Psychrometrics psychrometrics = new PsychroLib.Psychrometrics(PsychroLib.UnitSystem.SI);
+            if (psychrometrics == null)
+            {
+                return false;
+            }
+
+            humidityRatio = psychrometrics.GetHumRatioFromTWetBulb(dryBulbTemperature, wetBulbTemperature, pressure);
+            relativeHumidity = psychrometrics.GetRelHumFromHumRatio(dryBulbTemperature, humidityRatio, pressure)*100;
+            dewPointTemperature = psychrometrics.GetTDewPointFromHumRatio(dryBulbTemperature, humidityRatio, pressure);
+            vapourPressure = psychrometrics.GetVapPresFromHumRatio(humidityRatio, pressure);
+            enthalpy = psychrometrics.GetMoistAirEnthalpy(dryBulbTemperature, humidityRatio);
+            specificVolume = psychrometrics.GetMoistAirVolume(dryBulbTemperature, humidityRatio, pressure);
+            degreeSaturation = psychrometrics.GetDegreeOfSaturation(dryBulbTemperature, humidityRatio, pressure);
+
+            return true;
+        }
+
+
+        /// <summary>
+        ///Utility function to calculate humidity ratio, wet-bulb temperature, relative humidity,
+        /// vapour pressure, moist air enthalpy, moist air volume, and degree of saturation of air given
+        /// ry-bulb temperature, dew-point temperature, and pressure.
+        /// *The degree of saturation (i.e humidity ratio of the air / humidity ratio of the air at saturationat the same temperature and pressure)
+        /// </summary>
+        /// <param name="dryBulbTemperature">Dry bulb temperature [°C]</param>
+        /// <param name="dewPointTemperature">ew Point Temperature [°C]</param>
+        /// <param name="pressure">Atmospheric pressure [Pa]</param>
+        /// <param name="humidityRatio">Humidity Ratio [kg_waterVapor/kg_dryAir]</param>
+        /// <param name="wetBulbTemperature">Wet bulb temperature [°C]</param>
+        /// <param name="relativeHumidity">Relative humidity (0 - 100) [%]</param>
+        /// <param name="vapourPressure">Saturation Vapour Pressure [Pa]</param>
+        /// <param name="enthalpy">Enthalpy [J/kg]</param>
+        /// <param name="specificVolume">Specific Volume [m³/kg]</param>
+        /// <param name="degreeSaturation">Degree of saturation [unitless]</param>
+        /// <returns>list</returns>
+        public static bool CalcPsychrometrics_ByTDewPoint(double dryBulbTemperature, double dewPointTemperature, double pressure, out double humidityRatio, out double wetBulbTemperature, out double relativeHumidity, out double vapourPressure, out double enthalpy, out double specificVolume, out double degreeSaturation)
+        {
+            humidityRatio = double.NaN;
+            relativeHumidity = double.NaN;
+            wetBulbTemperature = double.NaN;
+            vapourPressure = double.NaN;
+            enthalpy = double.NaN;
+            specificVolume = double.NaN;
+            degreeSaturation = double.NaN;
+
+            PsychroLib.Psychrometrics psychrometrics = new PsychroLib.Psychrometrics(PsychroLib.UnitSystem.SI);
+            if (psychrometrics == null)
+            {
+                return false;
+            }
+
+            humidityRatio = psychrometrics.GetHumRatioFromTDewPoint(dewPointTemperature, pressure);
+            wetBulbTemperature = psychrometrics.GetTWetBulbFromHumRatio(dryBulbTemperature, humidityRatio, pressure);
+            relativeHumidity = psychrometrics.GetRelHumFromHumRatio(dryBulbTemperature, humidityRatio, pressure)*100;
+            vapourPressure = psychrometrics.GetVapPresFromHumRatio(humidityRatio, pressure);
+            enthalpy = psychrometrics.GetMoistAirEnthalpy(dryBulbTemperature, humidityRatio);
+            specificVolume = psychrometrics.GetMoistAirVolume(dryBulbTemperature, humidityRatio, pressure);
+            degreeSaturation = psychrometrics.GetDegreeOfSaturation(dryBulbTemperature, humidityRatio, pressure);
+
+            return true;
+        }
+
     }
 }
